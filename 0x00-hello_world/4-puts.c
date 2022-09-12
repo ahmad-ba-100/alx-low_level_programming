0c43c5768c506068795fdc70f3339f4dcae0c383
Amadou BA <bamadoulife@gmail.com>
2022-09-12 09:49:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 
 
-
 /**
 
 * main - print the string in the put function
@@ -21,8 +20,10 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
+
+
