75783d6b1a34081ddffdaaf8756763e2730c18d1
Amadou BA <bamadoulife@gmail.com>
2022-09-24 12:52:23 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,6 @@ int _putchar(char c);
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
-
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
